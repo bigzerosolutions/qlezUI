77cbcb6cc9e86a0ff4afa26cb121d76e36d80c36
=vishmealone91 <=vishnuprasad1990@gmail.com>
2016-12-03 18:38:55 +0530

    change

--- server.js
+++ server.js
@@ -89,12 +89,12 @@ var util = require('util');
 /*var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 */var log_stdout = process.stdout;
 var baseAPIurl = 'http://localhost:8080/AngularJSWithRestful';
-
+/*
 console.log = function(d) { //
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
-
+*/
 /******************************************* Login Section *********************************************/
 
 app.post('/loginAuth', function(req , res) 
